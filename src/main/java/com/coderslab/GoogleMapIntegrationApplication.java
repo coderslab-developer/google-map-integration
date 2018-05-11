7033b9f4b468de49f1f981584b1a4da733b5f33d
zubayer-ahamed-m4 <zubayer.ahamed@metafour.com>
2018-05-11 22:46:24 +0600

    first commit

---
+++ src/main/java/com/coderslab/GoogleMapIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.coderslab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapIntegrationApplication.class, args);
+	}
+}
